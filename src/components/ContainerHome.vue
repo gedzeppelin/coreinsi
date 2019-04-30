e12eb428cd0a70ef5fea8f3bebea5a89ca377d93
Gedy Palomino <gedy.palomino@gmail.com>
2019-04-29 23:05:38 -0500

    First improvisation of infinite scroll.
    It needs to be improved a lot. Need responsivity too.

--- src/components/ContainerHome.vue
+++ src/components/ContainerHome.vue
@@ -4,57 +4,68 @@
       <div class="holder" ref="holder">
 
         <div class="slide home1">
-            <div class="home1-text" v-if="holderPosition == 0">
-              <b>
-                <p class="home-a home1-p1">
-                  {{ $t('home1-1st-p') }}
-                </p>
-                <p class="home-a home1-p2">
-                  {{ $t('home1-2nd-p') }}
-                </p>
+          <div class="home1-text" v-if="animationPosition == 0">
+            <b>
+              <p class="home-a home1-p1">
+                {{ $t('home1-1st-p') }}
+              </p>
+              <p class="home-a home1-p2">
+                {{ $t('home1-2nd-p') }}
+              </p>
 
-                <p class="home-a home1-city">
-                  <font-awesome-icon :icon="['fas', 'map-marker-alt']" /> AYACUCHO, HUAMANGA, PERÚ
-                </p>
-              </b>
+              <p class="home-a home1-city">
+                <font-awesome-icon :icon="['fas', 'map-marker-alt']" /> AYACUCHO, HUAMANGA, PERÚ
+              </p>
+            </b>
 
-              </br>
+            </br>
 
-              <button type="button" class="btn-round btn-fb-lr-gp home-a home1-btn" @click="scrollToComponent('c-about')">
-                {{ $t('home-button-info') }}
-                <font-awesome-icon :icon="['fas', 'arrow-right']" />
-              </button>
+            <button type="button" class="btn-round btn-fb-lr-gp home-a home1-btn" @click="scrollToComponent('c-about')">
+              {{ $t('home-button-info') }}
+              <font-awesome-icon :icon="['fas', 'arrow-right']" />
+            </button>
 
-            </div>
+          </div>
         </div>
 
         <div class="slide home2">
-            <div class="home2-text" v-if="holderPosition == 1">
-              <b>
-                <p class="home-a home2-p">
-                  {{ $t('home2-1st-p') }}
-                </p>
-              </b>
+          <div class="home2-text" v-if="animationPosition == 1">
+            <b>
+              <p class="home-a home2-p">
+                {{ $t('home2-1st-p') }}
+              </p>
+            </b>
 
-              <img src="../assets/logo-unsch.png" width="20%" class="home-a home2-logo" />
+            <img src="../assets/logo-unsch.png" width="20%" class="home-a home2-logo" />
 
-              <div class="home-a home2-buttons">
-                <button type="button" class="btn-round btn-fb-lr-pw home2-btn" @click="scrollToComponent('c-schedule')">
-                  {{ $t('home2-button') }}
-                  <font-awesome-icon :icon="['fas', 'arrow-right']" />
-                </button>
-                <button type="button" class="btn-round btn-fb-lr-pw home2-btn" @click="scrollToComponent('c-about')">
-                  {{ $t('home-button-info') }}
-                  <font-awesome-icon :icon="['fas', 'arrow-right']" />
-                </button>
-              </div>
+            <div class="home-a home2-buttons">
+              <button type="button" class="btn-round btn-fb-lr-pw home2-btn" @click="scrollToComponent('c-schedule')">
+                {{ $t('home2-button') }}
+                <font-awesome-icon :icon="['fas', 'arrow-right']" />
+              </button>
+              <button type="button" class="btn-round btn-fb-lr-pw home2-btn" @click="scrollToComponent('c-about')">
+                {{ $t('home-button-info') }}
+                <font-awesome-icon :icon="['fas', 'arrow-right']" />
+              </button>
             </div>
+          </div>
         </div>
 
         <div class="slide home3">
-            <div class="" v-if="holderPosition == 2">
-
-            </div>
+          <div class="home3-text" v-if="animationPosition == 2">
+            <b>
+              <p class="home-a">
+                FRESI
+              </p>
+              <p class="home-a">
+                OF
+              </p>
+              <p class="home-a">
+                MRD
+              </p>
+              &#x1F648;
+            </b>
+          </div>
         </div>
 
       </div>
@@ -75,35 +86,54 @@ export default {
   data: function() {
     return {
       holderPosition: 0,
+      animationPosition: 0
     };
   },
   methods: {
     scrollXHomeLeft: function() {
-      var slider = this.$refs.slider;
+      let slider = this.$refs.slider;
+      let holder = this.$refs.holder;
 
-      this.holderPosition = this.holderPosition == 0 ? 2 : this.holderPosition - 1;
+      this.holderPosition = this.holderPosition == 0 ? holder.childElementCount - 1 : this.holderPosition - 1;
 
       this.$nextTick(function(){
         slider.scroll({
-          left: this.$refs.holder.children[this.holderPosition].offsetLeft,
+          left: holder.children[this.holderPosition].offsetLeft,
           behavior: 'smooth'
         });
       });
     },
 
     scrollXHomeRight: function() {
-      var slider = this.$refs.slider;
+      let slider = this.$refs.slider;
+      let holder = this.$refs.holder;
 
-      this.holderPosition = this.holderPosition < this.$refs.holder.childElementCount - 1 ? this.holderPosition + 1 : 0;
+      this.holderPosition = this.holderPosition < holder.childElementCount - 1 ? this.holderPosition + 1 : 0;
 
       this.$nextTick(function(){
         slider.scroll({
-          left: this.$refs.holder.children[this.holderPosition].offsetLeft,
+          left: holder.children[this.holderPosition].offsetLeft,
           behavior: 'smooth'
         });
       });
     },
+
+    onScroll: function() {
+      if(this.$refs.slider.scrollLeft.between(this.$refs.holder.children[this.holderPosition].offsetLeft - 3, this.$refs.holder.children[this.holderPosition].offsetLeft + 3, true)){
+        this.animationPosition = this.holderPosition;
+      } else{
+        this.animationPosition = -1;
+      }
+    }
   },
+  mounted() {
+    // Register an event listener when the Vue component is ready
+    this.$refs.slider.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    // Unregister the event listener before destroying this Vue instance
+    this.$refs.slider.removeEventListener('scroll', this.onScroll)
+  }
 }
 </script>
 
@@ -180,11 +210,12 @@ export default {
     margin-top: 30vh;
   }
   .home1-p1{
-    animation-delay: .55s;
+    // animation-delay: .55s;
   }
   .home1-p2{
-    animation-delay: .8s;
     margin-bottom: 0px;
+    animation-delay: .25s;
+    // animation-delay: .8s;
   }
   .home1-city{
     display: inline-block;
@@ -193,11 +224,13 @@ export default {
     background: $black;
     font-size: .55em;
     margin-bottom: 1.25em;
-    animation-delay: 1.15s;
+    animation-delay: .6s;
+    // animation-delay: 1.15s;
   }
   .home1-btn{
     display: inline-block;
-    animation-delay: 1.225s;
+    animation-delay: .70s;
+    // animation-delay: 1.225s;
   } //·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·|
 
 
@@ -214,17 +247,19 @@ export default {
     margin-top: 24vh;
   }
   .home2-p{
-    animation-delay: .55s;
+    // animation-delay: .55s;
   }
   .home2-logo{
     display: block;
     margin: 0 auto;
     margin-bottom: .25em;
-    animation-delay: .85s;
+    animation-delay: .3s;
+    // animation-delay: .85s;
   }
   .home2-buttons{
     text-align: center;
-    animation-delay: 1s;
+    animation-delay: .55s;
+    // animation-delay: 1s;
   }
   .home2-btn{
     vertical-align: middle;
@@ -239,6 +274,12 @@ export default {
   .home3{
     background: url("../assets/home3.jpg");
     @include background-fill;
+  }
+  .home3-text{
+    font-size: 4.5em;
+    text-align: center;
+    margin-top: 24vh;
+    color: black;
   } //=========================================================================\
 
 
--- src/main.js
+++ src/main.js
@@ -38,6 +38,13 @@ var i18n = new VueI18n({
   messages: { es, en } //set locale messages
 })
 
+Number.prototype.between = function(a, b, inclusive) {
+  var min = Math.min(a, b),
+    max = Math.max(a, b);
+
+  return inclusive ? this >= min && this <= max : this > min && this < max;
+}
+
 new Vue({
   i18n,
   router,
