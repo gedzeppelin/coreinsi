b15f1ca7950e6ac72d0ae70cde29a8ae12d23d11
Gedy Palomino <gedy.palomino@gmail.com>
2019-05-01 16:00:05 -0500

    Main responsivity (1/2)
    Containers responsivity to implement

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <header v-bind:class="{ 'scrolled-y': isScrolled }">
-      <span class="header-logo">
+    <header v-bind:class="{ 'scrolled-y': isScrolled, 'expanded': isTogleAux }">
+      <div class="burger-nav" @click="auux">
+          <span class="line" ref="line1"></span>
+          <span class="line" ref="line2"></span>
+          <span class="line line-3" ref="line3"></span>
+      </div>
+
+      <span class="header-logo header-logo-e" @click="scrollToComponent('c-home')" v-bind:class="{ 'active': position == 0 }" v-if="isTogleAux">
+        {{ $t('app-1st-li') }}
+      </span>
+
+      <span class="header-logo" v-else>
         XII COREINSI
       </span>
 
-      <nav>
-        <li @click="scrollToComponent('c-home')" v-bind:class="{ 'active': position == 0 }" class="nav-item">
+      <nav v-bind:class="{ 'nav-open': isTogleAux }">
+        <li @click="scrollToComponent('c-home')" v-bind:class="{ 'active': position == 0 }" class="nav-item nav-item-r">
           {{ $t('app-1st-li') }}
         </li>
-        <li @click="scrollToComponent('c-about')" v-bind:class="{ 'active': position == 1 }" class="nav-item">
+        <li @click="scrollToComponent('c-about')" v-bind:class="{ 'active': position == 1 }" class="nav-item nav-item-r2">
           {{ $t('app-2nd-li') }}
         </li>
         <li @click="scrollToComponent('c-speakers')" v-bind:class="{ 'active': position == 2 }" class="nav-item">
@@ -21,7 +31,7 @@
         <li @click="scrollToComponent('c-pricing')" v-bind:class="{ 'active': position == 4 }" class="nav-item">
           {{ $t('app-5th-li') }}
         </li>
-        <li @click="scrollToComponent('c-contact')" v-bind:class="{ 'active': position == 5 }" class="nav-item">
+        <li @click="scrollToComponent('c-contact')" v-bind:class="{ 'active': position == 5 }" class="nav-item nav-item-rl">
           {{ $t('app-6th-li') }}
         </li>
       </nav>
@@ -48,6 +58,7 @@
   export default {
     data: function() {
       return {
+        isTogleAux: false,
         isEnActive: true,
         isScrolled: false,
         position: 0,
@@ -73,6 +84,16 @@
         this.isEnActive = this.isEnActive === true ? false : true;
         this.$i18n.locale = this.$i18n.locale === 'es' ? 'en' : 'es';
       },
+      auux: function() {
+        this.isTogleAux = this.isTogleAux === true ? false : true;
+        if(this.isTogleAux == true) {
+          this.$refs.line1.style = "transform:rotate(45deg);top:8px;background:gold;"
+          this.$refs.line2.style = "width:0;opacity:0;"
+          this.$refs.line3.style = "transform:rotate(-45deg);top:-8px;background:gold;"
+        } else {
+          this.$refs.line1.style = this.$refs.line3.style = this.$refs.line2.style = null;
+        }
+      }
     },
     mounted() {
       // Register an event listener when the Vue component is ready
@@ -87,7 +108,7 @@
 
 <style lang="scss">
   /****************************************************************************|
-  |* Global configuration ··················································· *|
+  |* Global configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
   |****************************************************************************/
   html, body{
     background: $white;
@@ -96,11 +117,11 @@
     margin: 0px;
     scroll-behavior: smooth;
     font: 100% $font-stack;
-  } //·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·|
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
 
 
   /****************************************************************************|
-  |* Header configuration ··················································· *|
+  |* Header configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
   |****************************************************************************/
   header{
     display: inline-flex;
@@ -114,17 +135,24 @@
     left: 0;
     font-size: 24px;
     z-index: 1;
-    @include transition-pd(padding, .5s);
-  }
-
-  .scrolled-y{
-    background: $nav-color2;
-    padding: .65% 0px;
-  } //·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·|
+    @include transition-pd(all, .5s);
+    &.scrolled-y{
+      background: $nav-color2;
+      padding: .65% 0px;
+    }
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
 
 
   /****************************************************************************|
-  |* Header logo ···························································· *|
+  |* Header resposive menu button ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+  |****************************************************************************/
+  .nav-responsive{
+    display: none;
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+  /****************************************************************************|
+  |* Header logo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
   |****************************************************************************/
   .header-logo{
     display: inline-block;
@@ -133,11 +161,11 @@
     margin-right: 0px;
     color: gold;
     @include unselectable;
-  } //·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·|
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
 
 
   /****************************************************************************|
-  |* Header navigation ······················································ *|
+  |* Header navigation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
   |****************************************************************************/
   nav{
     display: inline-flex;
@@ -166,11 +194,11 @@
       color: gold;
       font-size: 24px;
     }
-  } //·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·|
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
 
 
   /****************************************************************************|
-  |* Navigation bar language selector ······································· *|
+  |* Navigation bar language selector ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
   |****************************************************************************/
   .language-selector {
     display: inline-block;
@@ -202,17 +230,169 @@
     display: inline-block;
     color: white;
     @include unselectable;
-  } //·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·=·|
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
 
+
+  /****************************************************************************|
+  |* Navigation fixed button (to top) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+  |****************************************************************************/
   .nav-up-button{
     cursor: pointer;
     position: fixed;
     bottom: 2.5%;
     right: 1.5%;
     color: gold;
-  }
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
 
-  @media only screen and (max-width: 768px){
 
-  }
+  /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+  /****************************************************************************|
+  |* RESPONSIVITY 800px ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+  |****************************************************************************/
+  @media only screen and (max-width: 800px){
+    /**************************************************************************|
+    |* Global configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    html, body{
+      font-size: 60%;
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+    /**************************************************************************|
+    |* Header configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    header{
+      background: $nav-color-r;
+      padding: 17.5px 0px;
+      flex-wrap: wrap;
+      &.expanded{
+        background: $nav-color;
+      }
+      &.scrolled-y{
+        padding: 7.5px 0px;
+        background: $nav-color2;
+      }
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+    /**************************************************************************|
+    |* Header resposive menu button ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    .burger-nav {
+      position: inline-block;
+      margin: 0px;
+      margin-left: 2.5%;
+      margin-right: 13px;
+      width: auto;
+      cursor: pointer;
+    }
+    .line {
+      background: $white;
+      display: block;
+      width: 30px;
+      height: 4px;
+      margin-bottom: 4px;
+      position: relative;
+      top: 0;
+      transition: all ease-in-out .35s;
+      border-radius: 10px;
+      &-3{
+        margin-bottom: 0px;
+      }
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+    /**************************************************************************|
+    |* Header responsive logo/first-nav-item ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    .header-logo{
+      width: auto;
+      margin: 0px;
+      color: gold;
+      font-size: 18px;
+      @include transition-pdf(color, .15s, linear);
+      &-e{
+        color: $white;
+        cursor: pointer;
+      }
+      &.active{
+        color: gold;
+      }
+      &:hover{
+        color:gold;
+      }
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+    /**************************************************************************|
+    |* Header responsive navigation since 2nd nav-item ~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    nav{
+      display: inline-flex;
+      flex-direction: column;
+      justify-content: null;
+      align-items: null;
+      visibility: hidden;
+      opacity: 0;
+      max-height: 0px;
+      width: 100%;
+      order: 1;
+      @include transition-multi(opacity .35s ease-in, max-height .25s ease-out);
+    }
+    .nav-open{
+      opacity: 1;
+      visibility: visible;
+      max-height: 200px;
+    }
+    .nav-item{
+      display: inline-block;
+      padding: 5px 0;
+      width: auto;
+      font-size: 18px;
+      line-height: 21px;
+      @include transition-pdf(color, .15s, linear);
+      &.active{
+        color: gold;
+        font-size: 18px;
+      }
+      &:hover{
+        color: gold;
+        font-size: 18px;
+      }
+      &-r{
+        display: none;
+      }
+      &-r2{
+        padding-top: 10px;
+      }
+      &-rl{
+        padding-bottom: 0;
+      }
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+    /**************************************************************************|
+    |* Navigation bar language selector ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    .language-selector {
+      width: auto;
+      margin-right: 2.25%;
+      font-size: 15px;
+      line-height: 21px;
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+    /**************************************************************************|
+    |* Navigation fixed button (to top) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *|
+    |**************************************************************************/
+    .nav-up-button{
+      bottom: 2.75%;
+      right: 5%;
+      font-size: 700%;
+    } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+  } //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|
+
+
+  /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 </style>
